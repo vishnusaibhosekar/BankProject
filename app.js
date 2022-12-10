127e7c73dc9cea8aee25be0d1f48f15baf6db193
Vishnu Sai Bhosekar <vishnusaibhosekar@gmail.com>
2022-12-10 17:53:44 -0500

    adds transaction ejs

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const connection = mysql.createConnection({
 	host     : 'localhost',
 	user     : 'root',
 	password : '',
-	database : 'BankProject'
+	database : 'bank_db'
 });
 
 const app = express();
@@ -41,13 +41,12 @@ app.post('/auth', function(request, response) {
     
 	let username = request.body.username;
 	let password = request.body.password;
+	console.error(username+" "+password);
     
 	if (username && password) {
-        
 		connection.query('SELECT * FROM auth WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
-
 			if (error) throw error;
-            
+            console.error(JSON.stringify(results));	
 			if (results.length > 0) {
                 if(results[0]['role'] === 'admin'){
 				request.session.loggedin = true;
@@ -59,7 +58,6 @@ app.post('/auth', function(request, response) {
 					console.log("in else")
 					request.session.loggedin = true;
 					request.session.username = username;
-					
 					response.redirect(`/userdash/${username}`);
 				}
 			} else {
@@ -81,48 +79,40 @@ app.get('/dashboard', async function(request, response) {
 				if (error) throw error;
 				
 				if (results.length > 0) {
-					if(results[0]['role'] === 'admin'){
 					fetchedBranches = results
 					await response.render('pages/dashboard',{
 						branchData: fetchedBranches
 				});
-				}	
-				} else {
-					await response.render('pages/dashboard',{
-						branchData: []
-					})
 				}			
 				// response.end();
 			});
-	}
- else {
+	} else {
+		console.error('Failed to render dashboard');
 		response.render('/');
 	}
 });
 
-app.get('/transaction', async function(request, response) {
-	response.render("pages/transaction",{
-		
+app.get('/transaction/:account_number', async function(request, response) {
+	let accNum = request.params.account_number;
+	response.render("pages/transaction/${}",{
+		account_number: accNum
 	})
 });
 
 app.get('/userdash/:username', async function(request, response) {
 	if (request.session.loggedin) {
 		let transacs;
-        	await connection.query(`SELECT * FROM customer cs inner join customer_account_access ca on cs.cssn = ca.cssn inner join customer_transaction ct on cs.cssn=ct.cssn inner join bank_account ba on ca.account_number=ba.account_number and cs.cssn=${request.params.username};`, async function(error, results, fields) {
+        	await connection.query(`SELECT * FROM customer cs inner join customer_account_access ca on cs.cust_ssn = ca.cust_ssn inner join customer_transaction ct on cs.cust_ssn=ct.cust_ssn inner join bank_account ba on ca.account_number=ba.account_number and cs.cust_ssn=${request.params.username};`, async function(error, results, fields) {
 				if (error) throw error;
 				
 				if (results.length > 0) {
 					transacs = results
 					await response.render('pages/usertransactions',{
 						userTransacs: transacs
-				});
-					
-				} 		
-				// response.end();
+					});
+				}
 			});
-	}
- else {
+	} else {
 		response.render('/');
 	}
 });
@@ -131,13 +121,13 @@ app.get('/bank/:id', function(request,response){
 	if(request.session.loggedin){
 		let accountsInBranch;
 		const bankid = request.params.id
-		 connection.query(`SELECT c.cssn, cu.cname, b.account_number, b.balance, b.branch_id, br.branch_name FROM customer_account_access c inner join bank_account b on b.account_number = c.account_number inner join customer cu on cu.cssn = c.cssn inner join branch br on br.branch_id = b.branch_id and b.branch_id=${bankid};`,  function(error, results, fields) {
+		connection.query(`SELECT c.cust_ssn, cu.cust_name, b.account_number, b.balance, b.branch_id, br.branch_name FROM customer_account_access c inner join bank_account b on b.account_number = c.account_number inner join customer cu on cu.cust_ssn = c.cust_ssn inner join branch br on br.branch_id = b.branch_id and b.branch_id=${bankid};`,  function(error, results, fields) {
 			if (error) throw error;
 			
 			if (results.length > 0) {
 				accountsInBranch = results
 				console.log(accountsInBranch)
-				 response.render('pages/customers',{
+				response.render('pages/customers',{
 					accounts: accountsInBranch
 			});
 			} else {
@@ -146,8 +136,7 @@ app.get('/bank/:id', function(request,response){
 			}			
 			// response.end();
 		});
-	}
-	else{
+	} else{
 		response.render('/');
 	}
 
@@ -156,5 +145,5 @@ app.get('/bank/:id', function(request,response){
 const port = 3000;
 
 app.listen(port, () => {
-    console.log(`server is running on port ${port}`);
+	console.log(`server is running on port ${port}`);
 });
