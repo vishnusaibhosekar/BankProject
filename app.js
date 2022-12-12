a4d891037982b4ba4bd6e0c8cc55e3c438575a86
Vishnu Sai Bhosekar <vishnusaibhosekar@gmail.com>
2022-12-12 01:06:55 -0500

    adds transaction logic and minor UI changes

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const session = require('express-session');
 const path = require('path');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const { compareSync } = require('bcryptjs');
 
 const connection = mysql.createConnection({
 	host     : 'localhost',
@@ -13,6 +14,7 @@ const connection = mysql.createConnection({
 });
 
 const app = express();
+let username;
 
 app.set("view engine", "ejs");
 
@@ -41,12 +43,10 @@ app.post('/auth', function(request, response) {
     
 	let username = request.body.username;
 	let password = request.body.password;
-	console.error(username+" "+password);
     
 	if (username && password) {
-		connection.query('SELECT * FROM auth WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+		connection.query('select * from auth WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
 			if (error) throw error;
-            console.error(JSON.stringify(results));	
 			if (results.length > 0) {
                 if(results[0]['role'] === 'admin'){
 				request.session.loggedin = true;
@@ -54,8 +54,7 @@ app.post('/auth', function(request, response) {
                 
 				response.redirect('/dashboard');
 				}
-				else{
-					console.log("in else")
+				else {
 					request.session.loggedin = true;
 					request.session.username = username;
 					response.redirect(`/userdash/${username}`);
@@ -63,8 +62,7 @@ app.post('/auth', function(request, response) {
 			} else {
                 response.render('pages/login');
 				console.error('Incorrect Username and/or Password!');
-			}			
-			response.end();
+			}
 		});
 	} else {
 		response.render('pages/login');
@@ -75,74 +73,116 @@ app.post('/auth', function(request, response) {
 app.get('/dashboard', async function(request, response) {
 	if (request.session.loggedin) {
 		let fetchedBranches;
-        	await connection.query('SELECT * FROM branch', async function(error, results, fields) {
+        	await connection.query('select * from branch', async function(error, results, fields) {
 				if (error) throw error;
 				
 				if (results.length > 0) {
-					fetchedBranches = results
+					fetchedBranches = results;
 					await response.render('pages/dashboard',{
 						branchData: fetchedBranches
 				});
-				}			
-				// response.end();
+				}
 			});
 	} else {
 		console.error('Failed to render dashboard');
-		response.render('/');
+		response.render('pages/login');
 	}
 });
 
 app.get('/transaction/:account_number', async function(request, response) {
+	username = request.params.username
 	let accNum = request.params.account_number;
 	response.render("pages/transaction",{
 		account_number: accNum
 	})
 });
 
-app.post('/transact', function(request, response) {
-	console.log("X "+request.query.JSON);
-	// let accNum = request.params.account_number;
-	// response.redirect("pages/transaction",{
-	// 	account_number: accNum
-	// })
+app.post('/maketransaction', async function(request, response) {
+	await connection.query(`select * from customer cs inner join customer_account_access ca on cs.cust_ssn = ca.cust_ssn inner join customer_transaction ct on cs.cust_ssn=ct.cust_ssn inner join bank_account ba on ca.account_number=ba.account_number and ba.account_number=${request.body.accNum};`, async function(error, results, fields) {
+		if (error) throw error;
+		if (results.length > 0) {
+			console.log(request.body);
+			console.log(results);
+			var transType = request.body.type;
+			var transId = transType.toUpperCase() + "2022110711558774310283" + (Math.floor(Math.random()*90000) + 10000);
+			var charge = 0;
+			var transDate = new Date().toISOString().slice(0, 10);
+			var transCode = transType.toUpperCase();
+			var amount = request.body.amount;
+			var today = new Date();
+			var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+			var accNum = request.body.accNum;
+			var ssn = results[0]['cust_ssn'];
+			if(request.body.type === 'CD') {
+				connection.query(`insert into customer_transaction(transaction_id, charge, transaction_date, code, amount, account_number, transaction_hour, cust_ssn) values ('${transId}', ${charge}, '${transDate}', '${transCode}', ${amount}, ${accNum}, '${time}', ${ssn});`, function(error, results, fields) {
+					if (error) throw error;
+					console.log("1 transaction inserted");
+				});
+				connection.query(`update bank_account SET balance=balance+${amount} where account_number=${accNum};`, function(error, results, fields) {
+					if (error) throw error;
+					console.log("Account balance updated(Deposit)");
+				});
+				response.redirect(`/userdash/${ssn}`);
+			} else {
+				connection.query(`select * from bank_account where account_number=${accNum};`, function(error, results, fields) {
+					if (error) throw error;
+					console.log(results[0]['balance']);
+					var curr_balance = results[0]['balance'];
+					if(curr_balance<amount) {
+						throw new Error('Not enough balance');
+					} else {
+						console.log("Withdrawal in progress");
+						connection.query(`insert into customer_transaction(transaction_id, charge, transaction_date, code, amount, account_number, transaction_hour, cust_ssn) values ('${transId}', ${charge}, '${transDate}', '${transCode}', ${amount}, ${accNum}, '${time}', ${ssn});`, function(error, results, fields) {
+							if (error) throw error;
+							console.log("1 transaction inserted");
+						});
+						connection.query(`update bank_account SET balance=balance-${amount} where account_number=${accNum};`, function(error, results, fields) {
+							if (error) throw error;
+							console.log("Account balance updated(Withdraw)");
+						});
+						response.redirect(`/userdash/${ssn}`);
+					}
+				});
+			}
+		}
+	});		
 });
 
-app.get('/userdash/:username', async function(request, response) {
+app.get('/userdash/:cust_ssn', async function(request, response) {
 	if (request.session.loggedin) {
 		let transacs;
-        	await connection.query(`SELECT * FROM customer cs inner join customer_account_access ca on cs.cust_ssn = ca.cust_ssn inner join customer_transaction ct on cs.cust_ssn=ct.cust_ssn inner join bank_account ba on ca.account_number=ba.account_number and cs.cust_ssn=${request.params.username};`, async function(error, results, fields) {
+        	await connection.query(`select * from customer cs inner join customer_account_access ca on cs.cust_ssn = ca.cust_ssn inner join customer_transaction ct on cs.cust_ssn=ct.cust_ssn inner join bank_account ba on ca.account_number=ba.account_number and cs.cust_ssn=${request.params.cust_ssn} order by ct.transaction_date desc,ct.transaction_hour desc;`, async function(error, results, fields) {
 				if (error) throw error;
 				
 				if (results.length > 0) {
 					transacs = results
 					await response.render('pages/usertransactions',{
-						userTransacs: transacs
+						userTransacs: transacs,
+						username: request.params.cust_ssn
 					});
 				}
 			});
 	} else {
-		response.render('/');
+		response.render('pages/login');
 	}
 });
 
 app.get('/bank/:id', function(request,response){
 	if(request.session.loggedin){
 		let accountsInBranch;
-		const bankid = request.params.id
-		connection.query(`SELECT c.cust_ssn, cu.cust_name, b.account_number, b.balance, b.branch_id, br.branch_name FROM customer_account_access c inner join bank_account b on b.account_number = c.account_number inner join customer cu on cu.cust_ssn = c.cust_ssn inner join branch br on br.branch_id = b.branch_id and b.branch_id=${bankid};`,  function(error, results, fields) {
+		const bankid = request.params.id;
+		connection.query(`select c.cust_ssn, cu.cust_name, b.account_number, b.balance, b.branch_id, br.branch_name from customer_account_access c inner join bank_account b on b.account_number = c.account_number inner join customer cu on cu.cust_ssn = c.cust_ssn inner join branch br on br.branch_id = b.branch_id and b.branch_id=${bankid};`,  function(error, results, fields) {
 			if (error) throw error;
 			
 			if (results.length > 0) {
-				accountsInBranch = results
-				console.log(accountsInBranch)
+				accountsInBranch = results;
 				response.render('pages/customers',{
 					accounts: accountsInBranch
 			});
 			} else {
 				response.render('pages/login');
 				console.error('Incorrect Username and/or Password!');
-			}			
-			// response.end();
+			}
 		});
 	} else{
 		response.render('/');
